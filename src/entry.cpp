ac63a4c5009caba35c401eeedfbee18ca4291f98
Laurent <seres@seres.eu.org>
2024-05-19 19:49:31 +0200

    bump version

--- src/entry.cpp
+++ src/entry.cpp
@@ -64,7 +64,7 @@ extern "C" __declspec(dllexport) AddonDefinition *GetAddonDef()
     AddonDef.Version.Major = 0;
     AddonDef.Version.Minor = 8;
     AddonDef.Version.Build = 3;
-    AddonDef.Version.Revision = 1;
+    AddonDef.Version.Revision = 2;
     AddonDef.Author = "Seres67";
     AddonDef.Description = "Adds a modular keyboard overlay to the UI.";
     AddonDef.Load = AddonLoad;
