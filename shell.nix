cb360528b11bbd0bca5c6ea03df8d207c702b63d
seres <seres@seres.eu.org>
2025-09-19 16:21:07 +0200

    shell working as expected!

--- shell.nix
+++ shell.nix
@@ -1,10 +1,15 @@
 {
   buildPackages,
   mkShell,
+  windows,
 }:
 mkShell {
   nativeBuildInputs = [
     buildPackages.stdenv.cc
     buildPackages.cmake
+  ];
+
+  buildInputs = [
+    windows.mingw_w64_headers
   ];
 }
