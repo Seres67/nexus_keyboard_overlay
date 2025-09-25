49f69e7592bcc199d1c318969fad18e7dc38ed5a
seres <seres@seres.eu.org>
2025-09-25 07:09:10 +0200

    removed unused file, added cmake lsp to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,12 +1,14 @@
 {
   mkShell,
   cmake,
+  cmake-language-server,
   clang-tools,
   bintools,
 }:
 mkShell {
   nativeBuildInputs = [
     cmake
+    cmake-language-server
     clang-tools
     bintools
   ];
